c372986ce214d621ed1a2bc687810d4f583651c6
Alex Guizar <alexguizar90@gmail.com>
2020-06-22 19:01:48 -0700

    Add CSS to normalize and arrange the grid. Add javascript to handle filtering

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 $('#gallery').rebox({ selector: 'a' });
+
+const filterGallery = function() {
+  const $input = $('.searchInput');
+  const $photos = $('#gallery a');
+
+  $input.on("keyup", function() {
+    const searchTerm = $(this).val();
+
+    $photos.each(function() {
+      if ($(this).attr('title').indexOf(searchTerm) === -1) {
+          $(this).parent().hide();
+      } else {
+        $(this).parent().show();
+      }
+    });
+  });
+};
+
+filterGallery();
